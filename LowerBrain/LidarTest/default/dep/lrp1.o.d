70fa2e44bb37ef206b7b764d378553eb8205ffaa
Michael Palacios <halo2master18@gmail.com>
2011-01-13 16:18:29 -0500

    Added test files?

---
+++ LowerBrain/LidarTest/default/dep/lrp1.o.d
@@ -0,0 +1,99 @@
+lrp1.o: ../lrp1.c ../hardware.h ../xhardware.h \
+  C:\webbotavrclib-1.17/a2d.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/iom640.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/iomxx0_1.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/interrupt.h \
+  C:\webbotavrclib-1.17/libdefs.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20081205\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  C:\webbotavrclib-1.17/iopin.h C:\webbotavrclib-1.17/errors.h \
+  c:/winavr-20081205/lib/gcc/../../avr/include/avr/eeprom.h \
+  C:\webbotavrclib-1.17/buffer.h C:\webbotavrclib-1.17/errors.h \
+  C:\webbotavrclib-1.17/libdefs.h C:\webbotavrclib-1.17/rprintf.h \
+  C:\webbotavrclib-1.17/servos.h C:\webbotavrclib-1.17/actuators.h \
+  C:\webbotavrclib-1.17/uart.h C:\webbotavrclib-1.17/_uart_common.h \
+  C:\webbotavrclib-1.17/buffer.h C:\webbotavrclib-1.17/switch.h \
+  C:\webbotavrclib-1.17/timer.h C:\webbotavrclib-1.17/core.h \
+  C:\webbotavrclib-1.17/scheduler.h C:\webbotavrclib-1.17/led.h \
+  C:\webbotavrclib-1.17/uart.h
+
+../hardware.h:
+
+../xhardware.h:
+
+C:\webbotavrclib-1.17/a2d.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/iom640.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/iomxx0_1.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/interrupt.h:
+
+C:\webbotavrclib-1.17/libdefs.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20081205\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+C:\webbotavrclib-1.17/iopin.h:
+
+C:\webbotavrclib-1.17/errors.h:
+
+c:/winavr-20081205/lib/gcc/../../avr/include/avr/eeprom.h:
+
+C:\webbotavrclib-1.17/buffer.h:
+
+C:\webbotavrclib-1.17/errors.h:
+
+C:\webbotavrclib-1.17/libdefs.h:
+
+C:\webbotavrclib-1.17/rprintf.h:
+
+C:\webbotavrclib-1.17/servos.h:
+
+C:\webbotavrclib-1.17/actuators.h:
+
+C:\webbotavrclib-1.17/uart.h:
+
+C:\webbotavrclib-1.17/_uart_common.h:
+
+C:\webbotavrclib-1.17/buffer.h:
+
+C:\webbotavrclib-1.17/switch.h:
+
+C:\webbotavrclib-1.17/timer.h:
+
+C:\webbotavrclib-1.17/core.h:
+
+C:\webbotavrclib-1.17/scheduler.h:
+
+C:\webbotavrclib-1.17/led.h:
+
+C:\webbotavrclib-1.17/uart.h:
